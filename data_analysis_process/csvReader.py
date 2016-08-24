e7a63dfae6c87f83710237da96fe06b2be1d1339
hushenglang <hushenglang@gmail.com>
2016-08-24 08:31:43 +0800

    data analysis practice

--- data_analysis_process/csvReader.py
+++ data_analysis_process/csvReader.py
@@ -1,7 +1,5 @@
 import unicodecsv
 
-enrollments = []
-
 ### the plain way to read the file, need to mannually close the file.
 # f = open('../csv_file/enrollments.csv', 'rb')
 # reader = unicodecsv.DictReader(f)
@@ -11,11 +9,39 @@ enrollments = []
 # f.close()
 
 ### the "with as" way to read the file, it will close the file automatically.
-with open('../csv_file/enrollments.csv', 'rb') as f:
-    reader = unicodecsv.DictReader(f)
-    ### reader will only be iteratered once.
-    for row in reader:
-        enrollments.append(row)
+# with open('../csv_file/enrollments.csv', 'rb') as f:
+#     reader = unicodecsv.DictReader(f)
+#     ### reader will only be iteratered once.
+#     for row in reader:
+#         enrollments.append(row)
 
-print enrollments[0]
-print enrollments[1]
+
+def read_csv(filename):
+    with open(filename, 'rb') as f:
+        reader = unicodecsv.DictReader(f)
+        return list(reader)
+
+enrollments = read_csv('../csv_file/enrollments.csv')
+daily_engagement = read_csv('../csv_file/daily_engagement.csv')
+project_submissions = read_csv('../csv_file/project_submissions.csv')
+
+print len(enrollments)
+
+unique_enrolled_students = set()
+for enrollment in enrollments:
+    unique_enrolled_students.add(enrollment['account_key'])
+print len(unique_enrolled_students)
+
+print len(daily_engagement)
+
+unique_engagement_students = set()
+for engagement_record in daily_engagement:
+    unique_engagement_students.add(engagement_record['acct'])
+print len(unique_engagement_students)
+
+print len(project_submissions)
+
+unique_project_submitters = set()
+for submission in project_submissions:
+    unique_project_submitters.add(submission['account_key'])
+print len(unique_project_submitters)
--- data_analysis_process/udacity_csv_test.py
+++ data_analysis_process/udacity_csv_test.py
@@ -4,15 +4,90 @@ enrollments_filename = '../csv_file/enrollments.csv'
 engagement_filename = '../csv_file/daily_engagement.csv'
 submissions_filename = '../csv_file/project_submissions.csv'
 
+
 def read_csv(filename):
     with open(filename, 'rb') as f:
         reader = unicodecsv.DictReader(f)
         return list(reader)
+
 
 enrollments = read_csv(enrollments_filename)
 engagement = read_csv(engagement_filename)
 submissions = read_csv(submissions_filename)
 
 print enrollments[0]
-print engagement[0]
+print engagement[0]['account_key']
 print submissions[0]
+
+enrollAccountKeys = map(lambda x: x['account_key'], enrollments)
+engagementAccountKeys = map(lambda x: x['account_key'], engagement)
+strangeAccountKeys = list(set(enrollAccountKeys).difference(set(engagementAccountKeys)))
+
+strangeEnrollments = [e for e in enrollments if
+                      e['account_key'] in strangeAccountKeys and e['join_date'] != e['cancel_date']]
+print 'result:', len(strangeEnrollments)
+print strangeEnrollments[0]
+
+
+###########################################################
+udacity_test_accounts = set([e['account_key'] for e in enrollments if e['is_udacity'] == 'True'])
+print 'udacity_test_accounts num:', len(udacity_test_accounts)
+print 'udacity_test_accounts', udacity_test_accounts
+
+def remove_udacity_accounts(data):
+    non_udacity_data = []
+    for data_point in data:
+        if data_point['account_key'] not in udacity_test_accounts:
+            non_udacity_data.append(data_point)
+    return non_udacity_data
+
+non_udacity_enrollments = remove_udacity_accounts(enrollments)
+non_udacity_engagement = remove_udacity_accounts(engagement)
+non_udacity_submissions = remove_udacity_accounts(submissions)
+
+print 'non_udacity_enrollments: ', len(non_udacity_enrollments)
+
+
+##########################################################
+paid_students = {}
+for enrollment in non_udacity_enrollments:
+    if (not enrollment['is_canceled'] or
+            enrollment['days_to_cancel'] > 7):
+        account_key = enrollment['account_key']
+        enrollment_date = enrollment['join_date']
+        if (account_key not in paid_students or
+                    enrollment_date > paid_students[account_key]):
+            paid_students[account_key] = enrollment_date
+print 'paid_students:', len(paid_students)
+
+
+##########################################################
+def remove_free_trial_cancels(data):
+    new_data = []
+    for data_point in data:
+        if data_point['account_key'] in paid_students:
+            new_data.append(data_point)
+    return new_data
+
+paid_enrollments = remove_free_trial_cancels(non_udacity_enrollments)
+paid_engagement = remove_free_trial_cancels(non_udacity_engagement)
+paid_submissions = remove_free_trial_cancels(non_udacity_submissions)
+print len(paid_enrollments)
+print len(paid_engagement)
+print len(paid_submissions)
+
+
+###########################################################
+def within_one_week(join_date, engagement_date):
+    time_delta = engagement_date - join_date
+    return time_delta.days < 7
+
+paid_engagement_in_first_week = []
+for engagement_record in paid_engagement:
+    account_key = engagement_record['account_key']
+    join_date = paid_students[account_key]
+    engagement_record_date = engagement_record['utc_date']
+    if within_one_week(join_date, engagement_record_date):
+        paid_engagement_in_first_week.append(engagement_record)
+
+print len(paid_engagement_in_first_week)
